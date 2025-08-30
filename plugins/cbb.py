b6757f08a96619759a3dd13a299269f4efc4b228
Madara <147428837+Madara1188@users.noreply.github.com>
2025-08-30 19:43:32 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -74,8 +74,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 f"<u><b>‼️ ᴀғᴛᴇʀ sᴇɴᴅɪɴɢ ᴀ sᴄʀᴇᴇɴsʜᴏᴛ ᴘʟᴇᴀsᴇ ɢɪᴠᴇ ᴜs sᴏᴍᴇ ᴛɪᴍᴇ ᴛᴏ ᴀᴅᴅ ʏᴏᴜ ɪɴ ᴛʜᴇ ᴘʀᴇᴍɪᴜᴍ ʟɪsᴛ.</b></u>"
             ),
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton('BUY NOW', url=SCREENSHOT_URL),
-                 InlineKeyboardButton('ᴄʟᴏꜱᴇ', callback_data='close')]
+                [InlineKeyboardButton(''ᴄʟᴏꜱᴇ', callback_data='close'),
+                 InlineKeyboardButton('ʙᴜʏ ɴᴏᴡ', url=SCREENSHOT_URL)]
             ])
         )
 
