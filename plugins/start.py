349afee59d1c491cc141f58061784f3cc0ba5eec
Madara <147428837+Madara1188@users.noreply.github.com>
2025-09-16 20:44:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -211,7 +211,7 @@ async def start_command(client: Client, message: Message):
     else:
         reply_markup = InlineKeyboardMarkup(
             [
-                    [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/Nova_Flix/50")],
+                    [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/Animes_Eternals")],
 
     [
                     InlineKeyboardButton("• ᴀʙᴏᴜᴛ", callback_data = "about"),
